fb8afdd07128116b74bddff8b54fed2ddc583e1d
quanbinn <quanbinn@126.com>
2013-04-17 09:44:42 +0800

    add root :to

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Uuarch::Application.routes.draw do
   resources :tasks do
     resources :steps
   end
+  
+  root :to => 'tasks#index'
 
 
   # The priority is based upon order of creation:
@@ -52,12 +54,6 @@ Uuarch::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
